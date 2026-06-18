8e492a510201faccba933ca31c3835efc046b6bb
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 17:27:58 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -14,3 +14,21 @@ export function login() {
       console.log("LOGIN ERROR", err);
     });
 }
+export function login() {
+  signInWithPopup(auth, provider)
+    .then((res) => {
+      const user = res.user;
+
+      console.log("LOGIN SUCCESS", user);
+
+      document.getElementById("userInfo").innerHTML = `
+        <div style="padding:10px;">
+          <img src="${user.photoURL}" width="30" style="border-radius:50%">
+          ${user.displayName}
+        </div>
+      `;
+    })
+    .catch((err) => {
+      console.log("LOGIN ERROR:", err);
+    });
+}
