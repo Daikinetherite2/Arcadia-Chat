8c439be6c8e883a62bf58383167ed7fdd3d7b46f
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 16:33:02 +0800

    Update chats.js

--- chats.js
+++ chats.js
@@ -6,3 +6,14 @@ chats/{chatId}/messages/{msgId}
 export function getChatId(a, b) {
   return [a, b].sort().join("_");
 }
+
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export async function sendMessage(chatId, uid, text) {
+  await addDoc(collection(db, "chats", chatId, "messages"), {
+    senderId: uid,
+    text,
+    createdAt: serverTimestamp()
+  });
+}
