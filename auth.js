ff292cafad4f95acec15399ba6b9770737c48637
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 17:28:30 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,19 +1,3 @@
-import { GoogleAuthProvider, signInWithPopup } 
-from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import { auth } from "./firebase.js";
-
-const provider = new GoogleAuthProvider();
-
-export function login() {
-  signInWithPopup(auth, provider)
-    .then((res) => {
-      console.log("LOGIN SUCCESS", res.user);
-    })
-    .catch((err) => {
-      console.log("LOGIN ERROR", err);
-    });
-}
 export function login() {
   signInWithPopup(auth, provider)
     .then((res) => {
