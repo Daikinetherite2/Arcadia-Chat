ee54aa110dc23378e9eeef52fa55e9a2858aef63
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 16:40:04 +0800

    Update calls.js

--- calls.js
+++ calls.js
@@ -3,3 +3,33 @@ calls/{chatId}
   answer
   caller
   receiver
+
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export async function startCall(chatId, offer) {
+  await setDoc(doc(db, "calls", chatId), {
+    offer
+  });
+}
+
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export function listenCall(chatId, callback) {
+  onSnapshot(doc(db, "calls", chatId), (docSnap) => {
+    callback(docSnap.data());
+  });
+}
+
+const pc = new RTCPeerConnection();
+
+async function createOffer() {
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  return offer;
+}
+
+import { login } from "./auth.js";
+
+window.login = login;
