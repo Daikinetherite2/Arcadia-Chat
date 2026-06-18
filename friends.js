2a6107964029a189a812f3f06636da6ba10a4bab
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 16:31:28 +0800

    Update friends.js

--- friends.js
+++ friends.js
@@ -1,2 +1,22 @@
 friends/{uid}/list/{friendUid}
   createdAt
+
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export async function addFriend(myUid, friendUid) {
+  await setDoc(doc(db, "friends", myUid, "list", friendUid), {
+    createdAt: Date.now()
+  });
+}
+
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export function listenFriends(uid, callback) {
+  onSnapshot(collection(db, "friends", uid, "list"), (snap) => {
+    const list = [];
+    snap.forEach(doc => list.push(doc.id));
+    callback(list);
+  });
+}
