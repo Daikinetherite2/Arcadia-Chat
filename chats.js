46954386697895e111d7a4d08a7db7937514ad1e
Daikinetherite2 <115567108+Daikinetherite2@users.noreply.github.com>
2026-06-18 16:33:53 +0800

    Update chats.js

--- chats.js
+++ chats.js
@@ -17,3 +17,39 @@ export async function sendMessage(chatId, uid, text) {
     createdAt: serverTimestamp()
   });
 }
+
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export function listenMessages(chatId, callback) {
+  const q = query(
+    collection(db, "chats", chatId, "messages"),
+    orderBy("createdAt")
+  );
+
+  onSnapshot(q, (snap) => {
+    const msgs = [];
+    snap.forEach(d => msgs.push(d.data()));
+    callback(msgs);
+  });
+}
+
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export function setOnline(uid, status) {
+  updateDoc(doc(db, "users", uid), {
+    online: status,
+    lastSeen: Date.now()
+  });
+}
+
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./firebase.js";
+
+export function setTyping(chatId, uid, isTyping) {
+  setDoc(doc(db, "typing", chatId + "_" + uid), {
+    typing: isTyping
+  });
+}
+
